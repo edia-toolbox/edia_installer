4ca1b8f0f2f320a876ef95f0c0c2f869768a1e8d
eioe <klotzsche@cbs.mpg.de>
2026-05-07 18:32:32 +0200

    bump version

--- Assets/Editor/EdiaInstaller.cs
+++ Assets/Editor/EdiaInstaller.cs
@@ -10,7 +10,7 @@ using UnityEngine;
 
 namespace Edia.Installer {
     
-    // v0.3.0 (2026-05-07, eioe)
+    // v0.4.0 (2026-05-07, eioe)
 
     public class EdiaInstaller : EditorWindow {
         // EDIA package IDs (as in their package.json)
